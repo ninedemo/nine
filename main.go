310e8db5265eb8c4ce3e7377051474b78cb4ec48
saribatir <31052699+saribatir@users.noreply.github.com>
2022-05-06 01:26:52 +1000

    no message

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+  "database/sql"
 	"io"
 	"log"
 	"net/http"
+
+  _ "github.com/lib/pq"
+)
+
+const (
+  host     = "localhost"
+  port     = 5432
+  user     = "postgres"
+  password = "your-password"
+  dbname   = "calhounio_demo"
 )
 
 func helloHandler(res http.ResponseWriter, req *http.Request) {
@@ -30,6 +41,23 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Go web app powered by Nine Publishing")
 }
 func main() {
+  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+      "password=%s dbname=%s sslmode=disable",
+      host, port, user, password, dbname)
+    db, err := sql.Open("postgres", psqlInfo)
+    if err != nil {
+      panic(err)
+    }
+    defer db.Close()
+
+    err = db.Ping()
+    if err != nil {
+      panic(err)
+    }
+
+    fmt.Println("Successfully connected!")
+
+
 	http.HandleFunc("/", defaultHandler)
 	http.HandleFunc("/hello", helloHandler)
 	err := http.ListenAndServe(":8080", nil)
